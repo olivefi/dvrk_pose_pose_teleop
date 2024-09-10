efd574ba5ad9b2f0487775fd3b9b68fb7dd2488a
olivefi <olivefi@ethz.ch>
2024-09-10 15:31:38 +0200

    edna momenta

--- src/DVRKTeleopInterface.cpp
+++ src/DVRKTeleopInterface.cpp
@@ -194,11 +194,6 @@ void DVRKTeleopInterface::processDVRKPoseForLegs(const geometry_msgs::TransformS
     Eigen::Quaterniond quatDiff = quatInit.inverse() * quat;
     Eigen::Vector3d euler = quatDiff.toRotationMatrix().eulerAngles(0, 1, 2);
     double rotationCmd = euler[2];
-    if (rotationCmd > M_PI) {
-      rotationCmd -= M_PI;
-    } else if (rotationCmd < -M_PI) {
-      rotationCmd += M_PI;
-    }
     geometry_msgs::TwistStamped twistDes;
     twistDes.twist.linear.x = xy_twist_scale_ * posErr[0];
     twistDes.twist.linear.y = xy_twist_scale_ * posErr[1];
